601b292567fd16b975e1516495a9878a6dd604b2
alex <asrocha92@gmail.com>
2021-10-24 11:32:20 -0300

    Create project HR-WORKER

---
+++ hr-worker-treiner/src/main/java/com/treiner/hrworker/HrWorkerTreinerApplication.java
@@ -0,0 +1,13 @@
+package com.treiner.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkerTreinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkerTreinerApplication.class, args);
+	}
+
+}
---
+++ hr-worker-treiner/src/test/java/com/treiner/hrworker/HrWorkerTreinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treiner.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerTreinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

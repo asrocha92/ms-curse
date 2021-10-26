3eefcdccd7909cb4ac66cab6fe5b931a355dd84c
alex <asrocha92@gmail.com>
2021-10-26 00:29:27 -0300

    tratando falhas na chamada de micro serviços com resilience4j

--- hr-payroll/src/main/java/com/treiner/hrpayroll/resource/PaymentResource.java
+++ hr-payroll/src/main/java/com/treiner/hrpayroll/resource/PaymentResource.java
@@ -1,6 +1,10 @@
 package com.treiner.hrpayroll.resource;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,17 +14,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.treiner.hrpayroll.entities.Payment;
 import com.treiner.hrpayroll.service.PaymentService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentResource {
 	
+	private static final String PAYMENT_SERVICE = "paymentService";
+	
 	@Autowired
 	private PaymentService service;
 	
-	
+	@CircuitBreaker(fallbackMethod = "orderFallback", name = PAYMENT_SERVICE)
 	@GetMapping(value = "/idworker/{idWorker}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long idWorker, @PathVariable Integer days) {
 		Payment payment =  service.getPayment(idWorker, days);
 		return ResponseEntity.ok(payment);
 	}
+	
+	public ResponseEntity<String> orderFallback(Exception e){
+        return ResponseEntity.status(400).body("Serviço indisponível");
+
+    }
+	
+	
 }
